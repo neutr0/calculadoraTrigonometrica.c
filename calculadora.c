ae255019194c5fbc7ef0c1471bae644bd5d6f7f2
JoaoPSantos0 <147274947+JoaoPSantos0@users.noreply.github.com>
2023-10-29 23:41:14 -0300

    calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -281,25 +281,37 @@ void SecanteR(){
   printf("\nA cotangente de %f rad's = %f\n", radiano, FcotgR);}
 
  void arcSenR() {
-     printf("Digite um valor entre -1 e 1: ");
+     printf("Digite um valor: ");
      scanf("%f", &radiano);
     angulo = radiano*180/PI;
     if (-1.0 <= angulo && angulo <= 1.0) {
         double Arcs = asin(angulo) * (180.0 / PI);
-        printf("\nO arco-seno de %lf é %.2f graus\n", radiano, Arcs);
+        printf("\nO arco-seno de %lf rad's é %.2f radianos\n", radiano, Arcs);
     } else {
-        printf("Valor fora do intervalo [-1, 1]\n");
+        
     }}
     
+    void ArcCos(){
+    double valor;
+     printf("Digite um valor: ");
+     scanf("%lf", &radiano);
+     angulo = radiano*180/PI;
+    if (-1.0 <= valor && valor <= 1.0){
+        double arco_cos = acos(valor) * (180.0 / PI);
+        printf("\nO arco-cosseno de %lf rad's é %.2f radianos\n", radiano, arco_cos);
+    }else {
+          
+    }} 
+
      void arcTgR() {
-     printf("Digite um valor entre -1 e 1: ");
+     printf("Digite um valor: ");
      scanf("%f", &radiano);
     angulo = radiano*180/PI;
     if (-1.0 <= angulo && angulo <= 1.0) {
         double Arctg = asin(angulo) * (180.0 / PI);
-        printf("\nO arco-seno de %lf é %.2f graus\n", radiano, Arctg);
+        printf("\nO arco-seno de %lf rad's é %.2f radianos\n", radiano, Arctg);
     } else {
-        printf("Valor fora do intervalo [-1, 1]\n");
+        
     }} 
 
 
