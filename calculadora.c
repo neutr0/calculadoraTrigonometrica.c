90166341176906b2151d924551c18373d775e037
JoaoPSantos0 <147274947+JoaoPSantos0@users.noreply.github.com>
2023-10-31 21:41:44 -0300

    calculadora.c

--- calculadora.c
+++ calculadora.c
@@ -1,4 +1,4 @@
-/* Olá, nós do grupo composto por: Lucas S., João Pedro S., Luan V., Mateus F. e Henrique M. inicialmente faríamos o "Cardápio de Carros";
+/* Olá, nós do grupo composto por: Lucas S., João Pedro S., Luan V., e Henrique M. inicialmente faríamos o "Cardápio de Carros";
 No entanto, notamos que não seria possível realizá-lo, uma vez que seria necessário utilizar banco de dados. Dessa forma, decidimos mudar
 o tema do trabalho para "Calculadora de Funções Trigonométricas, ou seja, desde a função seno até a função arco cotangente. 
 Atenciosamente. */
